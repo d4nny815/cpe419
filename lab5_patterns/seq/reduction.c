8ff12775bf1840ddcf2cb9aed6bb7e6cd7b082ed
danny <d4nny815@gmail.com>
2025-02-18 14:52:45 -0800

    seq part done. pads array to be a power of 2

--- lab5_patterns/seq/reduction.c
+++ lab5_patterns/seq/reduction.c
@@ -11,6 +11,7 @@ void init_arr(double* data, size_t n);
 void print_arr(double* data, size_t N);
 double reduction_neighbor(double* data, size_t N);
 double reduction_stride(double* data, size_t N);
+void segment_scan(double* data, size_t N, double* sum);
 
 int main(const int argc, const char** argv) {
     size_t N = 10;
@@ -21,17 +22,20 @@ int main(const int argc, const char** argv) {
     size_t bytes = N * sizeof(double);
     double* arr_neighbors = (double*)malloc(bytes);
     double* arr_stride = (double*)malloc(bytes);
-    if (!arr_stride || !arr_neighbors) {
+    double* arr_seg_scan = (double*)malloc(bytes);
+    if (!arr_stride || !arr_neighbors || !arr_seg_scan) {
         perror("malloc failed");
         exit(1);
     }
 
     init_arr(arr_neighbors, N);
     memcpy(arr_stride, arr_neighbors, bytes);
+    memcpy(arr_seg_scan, arr_neighbors, bytes);
 
     #ifdef DEBUG
-    print_arr(arr_neighbors, N);
-    print_arr(arr_stride, N);
+    // print_arr(arr_neighbors, N);
+    // print_arr(arr_stride, N);
+    print_arr(arr_seg_scan, N);
     #endif
 
     StartTimer();
@@ -40,17 +44,39 @@ int main(const int argc, const char** argv) {
 
     const double t_neighbor = GetTimer() / 1000.0;
 
-    
     StartTimer();
 
     double stride = reduction_stride(arr_stride, N);
 
     const double t_stride = GetTimer() / 1000.0;
 
-    printf("%zu, %lf, %lf, %lf, %lf\n", N, neighbor, t_neighbor, stride, t_stride);
+    
+    double sum;
+    size_t old_n = N;
+    if ((N & (N - 1)) != 0) N = 1 << ((size_t)log2(N) + 1);
+    bytes = N * sizeof(double);
+    arr_seg_scan = realloc(arr_seg_scan, bytes);
+    if (!arr_seg_scan) exit(1);
+    for (size_t i = old_n; i < N; i++) arr_seg_scan[i] = 0.0f;
+    
+    StartTimer();
 
+    segment_scan(arr_seg_scan, N, &sum);
+
+    const double t_scan = GetTimer() / 1000.0;
+
+    #ifdef DEBUG
+    printf("\n\nsum = %lf\n", sum);
+    print_arr(arr_seg_scan, old_n);
+    #endif
+
+
+    printf("%zu,%lf,%lf,%lf,%lf,%lf,%lf\n", old_n, 
+        neighbor, t_neighbor, stride, t_stride, sum, t_scan);
+ 
     free(arr_neighbors);
     free(arr_stride);
+    free(arr_seg_scan);
 
     return 0;
 }
@@ -100,10 +126,39 @@ double reduction_stride(double* data, size_t N) {
     return data[0]; 
 }
 
+void segment_scan(double* data, size_t n, double* sum) {
+    // reduction part
+    for (size_t stride = 1; stride < n; stride *= 2) {
+        for (size_t i = 2 * stride - 1; i < n; i += 2 * stride) {
+            data[i] += data[i - stride];
+        } 
+        #ifdef DEBUG
+        printf("After Stride %zu: ", stride);
+        print_arr(data, n);
+        #endif
+    }
+
+    *sum = data[n - 1];  
+    data[n - 1] = 0;    
+
+    // down sweep part
+    for (size_t stride = n / 2; stride > 0; stride /= 2) {
+        for (size_t i = stride * 2 - 1; i < n; i += stride * 2) {
+            double tmp = data[i - stride];
+            data[i - stride] = data[i];
+            data[i] += tmp;
+        }
+        
+    }
+    #ifdef DEBUG
+    print_arr(data, n);
+    #endif
+}
+
 void init_arr(double* data, size_t n) {
     for (size_t i = 0; i < n; i++) {
-        data[i] = rand();
-        // data[i] = i;
+        // data[i] = rand();
+        data[i] = i + 1;
     }
 }
 
