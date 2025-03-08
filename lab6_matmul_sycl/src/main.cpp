477b7fa0924afeaa3079b38a4cc3e2fff66bda55
danny <d4nny815@gmail.com>
2025-03-07 16:53:52 -0800

    benchmarking

--- lab6_matmul_sycl/src/main.cpp
+++ lab6_matmul_sycl/src/main.cpp
@@ -7,11 +7,11 @@
 using namespace sycl;
 using namespace std;
 
-void init_arrs(queue& q, float* arr, const size_t width, const size_t height) {
+void init_arrs(queue& q, float* arr, const size_t size) {
     std::time_t epoch_time = std::time(nullptr);
 
     q.submit([&](handler& h) {
-        h.parallel_for(range<1>(width * height), [=](auto i){
+        h.parallel_for(range<1>(size * size), [=](auto i){
             uint32_t seed = static_cast<uint32_t>(epoch_time * 6242878123) + i[0]; 
             seed = (1664525 * seed + 1013904223) % 0xFFFFFFFF;
 
@@ -22,50 +22,57 @@ void init_arrs(queue& q, float* arr, const size_t width, const size_t height) {
 }
 
 void matmul_seq(float const* arr1, float const* arr2, float* res_arr, 
-        const size_t width, const size_t height) {
+        const size_t size) {
 	
-    for (size_t row = 0; row < height; row++){
-        for(size_t col = 0; col < height; col++) {
-            size_t ind = row * height + col;
+    for (size_t row = 0; row < size; row++){
+        for(size_t col = 0; col < size; col++) {
+            size_t ind = row * size + col;
             res_arr[ind] = 0.0f;
-            for(size_t k = 0; k < width; k++){
-                res_arr[ind] += arr1[row * width + k] * arr2[k * height + col];
+            for(size_t k = 0; k < size; k++){
+                res_arr[ind] += arr1[row * size + k] * arr2[k * size + col];
             }
         }
 	}
 }
 
-void matmul_par(queue& q, float const* arr1, float const* arr2, float* res_arr, 
-        const size_t width, const size_t height) {
-	
+void matmul_par(queue& q, float* arr1, float* arr2, float* res_arr, 
+    const size_t size) {
+
     q.submit([&](handler& h) {
-        h.parallel_for(range<2>(height, height), [=](id<2> i){
-            auto row = i[0];
-            auto col = i[1];
-            
+        h.parallel_for(range<2>(size, size), [=](id<2> idx) {
+            auto row = idx[0];
+            auto col = idx[1];
+
             float sum = 0.0f;
-            for(size_t k = 0; k < width; k++){
-                sum += arr1[row * width + k] * arr2[k * height + col];
+            for (size_t k = 0; k < size; k++) {
+                sum += arr1[row * size + k] * arr2[k * size + col];
             }
-            res_arr[row * height + col] = sum;
+            res_arr[row * size + col] = sum;  
         });
     }).wait();
 }
 
-bool equal_arrs(float const* arr1, float const* arr2, 
+void equal_arrs(float const* arr1, float const* arr2, 
         const size_t width, const size_t height) {
     
-    constexpr float EPISLON = 1e-2; 
+    constexpr float EPISLON = 1; 
+    size_t num_errors = 0;
+    double max_error = 0.0;
+    
     for (size_t row = 0; row < height; row++){
         for(size_t col = 0; col < width; col++) {
-            if (std::abs(arr1[row * width + col] - arr2[row * width + col]) > EPISLON) {
+            double error = std::abs(arr1[row * width + col] - arr2[row * width + col]);
+            if (error > EPISLON) {
+                max_error = std::max(max_error, error);
+                num_errors++;
                 fprintf(stderr, "[%zu] %f != %f\n", row * width + col, 
                     arr1[row * width + col], arr2[row * width + col]);
-                return false;
             }
         }
 	}
-    return true;
+    if (num_errors > 0) printf("There were %zu errors out of %zu of %lf\n", 
+        num_errors, width * height, max_error);
+    else printf("Same Arrs\n");
 }
 
 void print_arr(float* arr1, size_t width, size_t height) {
@@ -80,8 +87,7 @@ void print_arr(float* arr1, size_t width, size_t height) {
 
 int main(void) {
     // queue q(default_selector_v, exception_handler);
-    constexpr size_t M = 10;
-    constexpr size_t N = 5;
+    constexpr size_t N = 1000;
 
     auto selector = default_selector_v;
     // auto selector = gpu_selector_v;
@@ -95,8 +101,8 @@ int main(void) {
         << q.get_device().get_info<info::device::name>() << "\n";
 
 
-    float* mat1 = malloc_shared<float>(M * N, q);
-    float* mat2 = malloc_shared<float>(M * N, q);
+    float* mat1 = malloc_shared<float>(N * N, q);
+    float* mat2 = malloc_shared<float>(N * N, q);
     if ((mat1 == nullptr) || (mat2 == nullptr)) {
         if (mat1 != nullptr) free(mat1, q);
         if (mat2 != nullptr) free(mat2, q);
@@ -105,12 +111,12 @@ int main(void) {
         return -1;
     }
 
-    init_arrs(q, mat1, M, N);
-    init_arrs(q, mat2, N, M);
+    init_arrs(q, mat1, N);
+    init_arrs(q, mat2, N);
 
-    // print_arr(mat1, M, N);
+    // print_arr(mat1, N, N);
     // printf("\n===========\n\n");
-    // print_arr(mat2, N, M);
+    // print_arr(mat2, N, N);
     // printf("\n===========\n\n");
 
     float* seq = (float*)malloc(N * N * sizeof(float));
@@ -124,8 +130,8 @@ int main(void) {
         return -1;
     }
 
-    matmul_seq(mat1, mat2, seq, M, N);
-    matmul_par(q, mat1, mat2, par, M, N);
+    matmul_seq(mat1, mat2, seq, N);
+    matmul_par(q, mat1, mat2, par, N);
 
     // print_arr(seq, N, N);
     // printf("\n===========\n\n");
